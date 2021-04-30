6a6b5e05a6be3aed575e344ef2de477573e5b974
Manuel Cocoba <manuelcocoba@gmail.com>
2021-04-29 21:38:26 -0700

    double stack to handle min

--- w3/1-minstack.js
+++ w3/1-minstack.js
@@ -5,7 +5,7 @@ class Node {
   }
 }
 
-class MinStack {
+class stack {
   constructor() {
     this.top = new Node();
     this.min = undefined;
@@ -49,11 +49,56 @@ class MinStack {
   }
 }
 
+class MinStack {
+  constructor() {
+    this.currentStack = new stack();
+    this.minStack = new stack();
+  }
+
+  push(item) {
+    if (this.minStack.peek() == undefined) {
+      this.minStack.push(item);
+    }
+
+    if (item <= this.minStack.peek()) {
+      this.minStack.push(item);
+    }
+    this.currentStack.push(item);
+  }
+
+  pop() {
+    if (this.currentStack.pop() === this.minStack.peek()) {
+      this.minStack.pop();
+    }
+    // this.currentStack.pop();
+  }
+
+  peek() {
+    return this.currentStack.peek();
+  }
+
+  getMin() {
+    return this.minStack.peek();
+  }
+}
+
+let minStackBasic = new MinStack();
+minStackBasic.push(-2);
+minStackBasic.push(0);
+minStackBasic.push(-3);
+console.log('-3', minStackBasic.getMin());
+minStackBasic.pop();
+console.log('0', minStackBasic.peek());
+console.log('-2', minStackBasic.getMin());
+
+console.log('------- CASE 2 -------');
 let minStack = new MinStack();
 minStack.push(-2);
 minStack.push(0);
+minStack.push(-6);
 minStack.push(-3);
-console.log('-3', minStack.getMin());
+minStack.push(-2);
+console.log('-6', minStack.getMin());
 minStack.pop();
-console.log('0', minStack.peek());
-console.log('-2', minStack.getMin());
+console.log('-3', minStack.peek());
+console.log('-6', minStack.getMin());
